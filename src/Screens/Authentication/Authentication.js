84699bfc4a54439f08d8c92820830be019ca2577
Billy <billyjcatchpole@gmail.com>
2020-01-18 09:37:09 +0000

    All component unit tests are now passing

--- src/Screens/Authentication/Authentication.js
+++ src/Screens/Authentication/Authentication.js
@@ -1,8 +1,19 @@
+//core react
 import React from "react"
+
+//Components
+import Logo from "../../Components/Logo/Logo"
+import Input from "../../Components/Input/Input"
 
 const Authentication = props => (
 
-    <div>
+    <div test-handle="container">
+
+        <Logo test-handle="logo"/>
+
+        <Input test-handle="input"/>
+
+        <img test-handle="image" alt="A" />
 
 
     </div>
