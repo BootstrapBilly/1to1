cb4c29f0f5cb6e40e29721684d006cbd0cbc095c
Billy <billyjcatchpole@gmail.com>
2020-01-28 16:50:13 +0000

    Moved calendar to an external component and tidied up the structure of the project, no tests needed as it is an external library

---
+++ src/Components/Calendar/Calendar.js
@@ -0,0 +1,36 @@
+import React from "react"
+
+//external
+import Calendar from 'react-calendar'
+
+//icons
+import arrow from "../../Assets/Icons/arrow.svg"
+
+//css
+import classes from "./Calendar.module.css"
+
+const Calendar_ = props => {
+
+    const dashboardProps = props.dashboardProps
+
+    return (
+
+        <Calendar value={new Date()}
+
+            prevLabel={<img src={arrow} alt="An arrow pointing left" className={classes.calendarArrowLeft} />} prev2Label={null}
+
+            navigationLabel={({ date, view, label }) => <p className={classes.calendarTitle}>{label}</p>}
+
+            nextLabel={<img src={arrow} alt="An arrow pointing right" className={classes.calendarArrowRight} />} next2Label={null}
+
+            tileClassName={classes.calendarTile} className={classes.Calendar}
+
+            showNeighboringMonth={false} maxDetail={"month"} minDetail={"month"}
+
+            onClickDay={() => console.log(dashboardProps)} />
+    )
+
+}
+
+
+export default Calendar_
--- src/Screens/Dashboard/Dashboard.js
+++ src/Screens/Dashboard/Dashboard.js
@@ -1,22 +1,13 @@
 //core react
 import React from "react"
 
-//external
-import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css';
-
-//icons
-import arrow from "../../Assets/Icons/arrow.svg"
-
-
 //components
 import Header from "../../Components/Header/Header"
+import Calendar from "../../Components/Calendar/Calendar"
 
 import classes from "./Dashboard.module.css"
 
 const Dashboard = props => {
-
-    const dashboardProps = props;
 
     return (
 
@@ -26,21 +17,7 @@ const Dashboard = props => {
 
             <div test-handle="next-client"></div>
 
-            <div className={classes.calendarWrapper} test-handle="calendar">
-
-                <Calendar value={new Date()}
-
-                    prevLabel={<img src={arrow} alt="An arrow pointing left" className={classes.calendarArrowLeft} />} prev2Label={null}
-
-                    navigationLabel={({ date, view, label }) => <p className={classes.calendarTitle}>{label}</p>}
-
-                    nextLabel={<img src={arrow} alt="An arrow pointing right" className={classes.calendarArrowRight} />} next2Label={null}
-                   
-                    tileClassName={classes.calendarTile} className={classes.Calendar}
-
-                    showNeighboringMonth={false} maxDetail={"month"} minDetail={"month"}
-
-                    onClickDay={() => console.log(dashboardProps)} /></div>
+            <div className={classes.calendarWrapper} test-handle="calendar"><Calendar dashboardProps={props}/></div>
 
         </div>
 
