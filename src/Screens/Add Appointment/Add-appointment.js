f75f2c961f985753bd114065b3501fb54e5178cf
Billy <billyjcatchpole@gmail.com>
2020-02-10 19:04:00 +0000

    Refactored add appointment, select length, found a navigation bug when pressing the back arrow on the add appointment screen

--- src/Screens/Add Appointment/Add-appointment.js
+++ src/Screens/Add Appointment/Add-appointment.js
@@ -1,47 +1,62 @@
+//react
 import React, { useState, useEffect } from "react"
+
+//css
 import classes from "./Add-appointment.module.css"
 
+//components
 import SelectLength from "./SelectLength/SelectLength"
 import SelectClient from "./SelectClient/SelectClient"
 import Header from "../../Containers/Header/Header"
 
+//redux hooks
 import {useSelector, useDispatch} from "react-redux"
 
+//redux actions
 import {addAppointment, reset} from "../../store/actions/Add Appointment/Add-Appointment-action"
 
 const AddToGrid = props => {
 
-    const dispatch = useDispatch()
+    /*//!
 
-    const cell = props.history.location.cell
-    const date = props.history.location.date
+Bug caused by the props not being passed in when going back
 
-    const [lengthChosen, setLengthChosen] = useState(null)
+   //! */
 
-    const selectedClient = useSelector(state => state.addAppointment.clientName)
-    const appointmentAdded = useSelector(state => state.addAppointment.appointmentAdded)
+    //_Config
+    const dispatch = useDispatch()//initialise the action dispatcher
+    const cell = props.history.location.cell//grab the first cell where the appointment starts from, from props
+    const date = props.history.location.date//grab the date passed in by props
 
+    //*states
+    const [lengthChosen, setLengthChosen] = useState(null)//holds the state which determines the length of the appointment (initially it hasn't been sey)
 
+    //-Selectors
+    const appointmentHolder = useSelector(state => state.addAppointment.clientName)//grab the appointment holder from the store, (set on the select client screen)
+    const appointmentAdded = useSelector(state => state.addAppointment.appointmentAdded)//listen for the appointment being added (dispatched when all info has been set)
+
+    //!Effects
     useEffect(()=> {
 
-        if(selectedClient && cell && lengthChosen && date) dispatch(addAppointment({clientName: selectedClient, cell:cell, date:date, appointmentLength:lengthChosen}))
+        //if all the necessary information has been submitted, dispatch an add appointment action with the relevant information
+        if(appointmentHolder && cell && lengthChosen && date) dispatch(addAppointment({clientName: appointmentHolder, cell:cell, date:date, appointmentLength:lengthChosen}))
 
+        //if the appointment has been added successfully
         if(appointmentAdded) {
 
-            props.history.push(`/calendar-date/${date}`)
-            dispatch(reset())
+            props.history.push(`/calendar-date/${date}`)//navigate back to the grid date where it was set
+            dispatch(reset())//and reset everything
 
         }
         
     })
-
 
     return (
 
         <React.Fragment>
 
             <Header text={lengthChosen ? "Select a client" : "Appointment length"} backArrow 
-            handleBack={()=> lengthChosen ? setLengthChosen(null) : props.history.goBack()}/>
+            handleBack={()=> lengthChosen ? setLengthChosen(null) : props.history.push(`/add-appointment`)}/>
 
             <div className={classes.container}>
 
--- src/Screens/Add Appointment/SelectLength/SelectLength.js
+++ src/Screens/Add Appointment/SelectLength/SelectLength.js
@@ -1,35 +1,46 @@
+//react
 import React from "react"
+
+//css
 import classes from "./selectLength.module.css"
 
+//redux hooks
 import { useSelector } from "react-redux"
 
 const SelectLength = props => {
 
     const appointments = useSelector(state => state.fetchAppointments.appointments)//get the appointment data fetched from the api
 
-    const currentCol = props.cell.slice(3, 4)
-    const currentRow = props.cell.slice(8)
+    const currentCol = props.cell.slice(3, 4)//extract the current column from the passed in cell
+    const currentRow = props.cell.slice(8)//extract the current row from the pass in cell
 
-    let options = [30, 45, 60]
+    let options = [30, 45, 60]//set the default options (30, 45, 60) - This is modified by the switch case below - 15 mins is always added by default (check the render method)
 
     const setOptions = () => {
 
-    switch (currentCol) {
+    switch (currentCol) {//switch the column of where the first cell of the appointment starts (the column of the cell where the user clicked to trigger this process)
 
-        case "1":
+        case "1": //the appointment starts in the first column
 
+            //if there as an appointment which starts in the next cell, on the same row, there is only space for a 15 min appointment (1 cell)
             if (appointments.find(item => item.col === (parseInt(currentCol) + 1).toString() && item.row === currentRow)) return options = []
+            //if there is an appointment which starts two cells away, on the same row, there is space for a 30 min appointment (2 cells)
             if (appointments.find(item => item.col === (parseInt(currentCol) + 2).toString() && item.row === currentRow)) return options = [30]
+            //if there is an appointment which starts three cells away, on the same row, there is space for a 45 min appointment (3 cells)
             if (appointments.find(item => item.col === (parseInt(currentCol) + 3).toString() && item.row === currentRow)) return options = [30, 45]
+
+            //otherwise, there is space for a 60 min appointment (4 cells)
 
             break;
 
-        case "2":
+        case "2": //if the appointment starts in the second column
 
             if (appointments.find(item => item.col === (parseInt(currentCol) + 1).toString() && item.row === currentRow)) return options = []
             if (appointments.find(item => item.col === (parseInt(currentCol) + 2).toString() && item.row === currentRow)) return options = [30]
+            //if there is an appointment which starts 3 cells away, on the next row (because this appointment starts from column 2), there is room for a 45 min appointment
             if (appointments.find(item => item.col === (parseInt(currentCol) - 1).toString() && item.row === (parseInt(currentRow) + 1).toString())) return options = [30, 45]
-
+            
+            //otherwise there is space for a 60  in appointment (4 cells)
             break;
 
         case "3":
